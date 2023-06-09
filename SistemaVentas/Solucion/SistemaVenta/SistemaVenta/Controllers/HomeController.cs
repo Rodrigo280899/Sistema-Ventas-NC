df431f5cb289d7fe0577a5807e9d15ccfc884bb9
samueloliver-sa <oliversanchez0307@gmail.com>
2023-06-09 17:52:01 -0500

    Pruebas KQB 17:51

--- SistemaVentas/Solucion/SistemaVenta/SistemaVenta/Controllers/HomeController.cs
+++ SistemaVentas/Solucion/SistemaVenta/SistemaVenta/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace SistemaVenta.Controllers
             return View();
         }
 
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
